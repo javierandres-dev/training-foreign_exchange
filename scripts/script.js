6e3eff57a70bd2a8377397c58e9bcfbc923b7336
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-01-15 17:52:43 -0500

    Training

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const d = document,
   itemsXpage = 10;
-let items = undefined,
+let tcrmh = undefined,
+  items = undefined,
   pages = undefined,
   page = undefined;
 
@@ -22,51 +23,45 @@ const getTRM = async () => {
   }
 };
 const printTRM = async () => {
-  const TRM = await getTRM(),
-    $today = d.getElementById("today"),
-    $history = d.getElementById("history"),
-    $span = $today.querySelector("span"),
-    $tbody = $history.querySelector("tbody");
-  console.log(pages);
-  $span.textContent = `${TRM[0].valor}`;
-  /*
-  $date.textContent = `${TRM[0].vigenciadesde}`;
-  TRM.forEach((trm) => {
-    const $tr = d.createElement("tr"),
-      $td1 = d.createElement("td"),
-      $td2 = d.createElement("td"),
-      $td3 = d.createElement("td"),
-      $td4 = d.createElement("td");
-    $td1.textContent = `${trm.valor}`;
-    $td2.textContent = `${trm.unidad}`;
-    $td3.textContent = `${trm.vigenciadesde}`;
-    $td4.textContent = `${trm.vigenciahasta}`;
-    $tr.appendChild($td1);
-    $tr.appendChild($td2);
-    $tr.appendChild($td3);
-    $tr.appendChild($td4);
-    $tbody.appendChild($tr);
-  });
-  */
-  printHistory(TRM, 990);
+  tcrmh = await getTRM();
+  //console.log(tcrmh);
+  const trm = tcrmh[0],
+    $value = d.getElementById("value"),
+    $date = d.getElementById("date"),
+    value =
+      trm.valor.substring(0, 1) +
+      "." +
+      trm.valor.substring(1, trm.valor.length).replace(".", ","),
+    date = trm.vigenciadesde.slice(0, 10).replaceAll("-", "/");
+  $value.textContent = `${value}`;
+  $date.textContent = `${date}`;
+  printHistory(0);
 };
-const printHistory = (trm, start) => {
+const printHistory = (start) => {
   let i = start;
   const end = start + itemsXpage,
     $history = d.getElementById("history"),
+    $caption = $history.querySelector("caption"),
     $tbody = $history.querySelector("tbody");
-  page = pages / itemsXpage;
-  console.log(page);
+  page = end / itemsXpage;
+  $caption.textContent = `Page ${page} of ${pages}`;
   for (i; i < end; i++) {
-    const $tr = d.createElement("tr"),
+    const trm = tcrmh[i],
+      value =
+        trm.valor.substring(0, 1) +
+        "." +
+        trm.valor.substring(1, trm.valor.length).replace(".", ","),
+      since = trm.vigenciadesde.slice(0, 10).replaceAll("-", "/"),
+      until = trm.vigenciadesde.slice(0, 10).replaceAll("-", "/"),
+      $tr = d.createElement("tr"),
       $td1 = d.createElement("td"),
       $td2 = d.createElement("td"),
       $td3 = d.createElement("td"),
       $td4 = d.createElement("td");
-    $td1.textContent = `${trm[i].valor}`;
-    $td2.textContent = `${trm[i].unidad}`;
-    $td3.textContent = `${trm[i].vigenciadesde}`;
-    $td4.textContent = `${trm[i].vigenciahasta}`;
+    $td1.textContent = `$${value}`;
+    $td2.textContent = `${trm.unidad}`;
+    $td3.textContent = `${since}`;
+    $td4.textContent = `${until}`;
     $tr.appendChild($td1);
     $tr.appendChild($td2);
     $tr.appendChild($td3);
