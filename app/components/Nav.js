a5b56f24f73a1fb197a85028c56cd5db535de22a
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-01-16 18:06:27 -0500

    Training

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,6 +1,7 @@
 "use strict";
 export default function Nav() {
   const $nav = document.createElement("nav");
+  $nav.classList.add("nav");
   $nav.innerHTML = `
 <a href="#/">home</a>
 <a href="#/money-exchange">money exchange</a>
