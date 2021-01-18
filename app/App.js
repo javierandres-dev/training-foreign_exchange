9505cae9e8cb163ddc5f358aec13b03c2838df77
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-01-17 22:24:18 -0500

    Training

--- app/App.js
+++ app/App.js
@@ -3,7 +3,6 @@ import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 import Aside from "./components/Aside.js";
 import Nav from "./components/Nav.js";
-import Footer from "./components/Footer.js";
 import Router from "./components/Router.js";
 export default function App() {
   const $body = document.querySelector("body");
@@ -12,6 +11,5 @@ export default function App() {
   $body.appendChild(Nav());
   $body.appendChild(Main());
   $body.appendChild(Aside());
-  $body.appendChild(Footer());
   Router();
 }
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,19 +1,20 @@
 "use strict";
 export default function Header() {
   const $header = document.createElement("header");
-  $header.classList.add("header");
+  $header.classList.add("dev");
   $header.innerHTML = `
-<h1>foreign exchange</h1>
-<h2>
-  Built by
+<h1>Foreign Exchange</h1>
+<p>
+  ©&nbsp;&nbsp;Built by
   <a
     href="https://www.javierandresgp.com"
     target="_blank"
     rel="noopener noreferrer"
-    >Javier Andrés GP</a
-  >
-</h2>
-<p>Found a bug or have an idea? Contact me</p>
+    >Javier Andrés GP
+  </a>
+  <br />
+  <span> Found a bug or have an idea? Contact me</span>
+</p>
   `;
   return $header;
 }
